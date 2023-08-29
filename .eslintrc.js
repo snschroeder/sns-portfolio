ee2ae9074a3a86724db3ad46ae1f0b8f938eafe5
sns <scott.schroeder@psaudio.com>
2023-08-29 12:02:20 -0600

    initial commit, configures eslint

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports =  {
+  root: true,
+  parser:  '@typescript-eslint/parser',  // Specifies the ESLint parser
+  extends:  [
+    'airbnb-typescript',
+  ],
+  parserOptions:  {
+    project: './tsconfig.json',
+    // tsconfigRootDir: __dirname,
+    ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
+    sourceType:  'module',  // Allows for the use of imports
+    ecmaFeatures:  {
+      jsx:  true,  // Allows for the parsing of JSX
+    },
+  },
+  rules:  {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+  },
+  settings:  {
+    react:  {
+      version:  'detect',  // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  },
+  plugins: [
+    'react',
+    'import',
+    'jsx-a11y',
+  ],
+};
