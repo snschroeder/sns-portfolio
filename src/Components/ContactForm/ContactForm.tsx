52f2cbf9e84f49042fba7130717731f0a210f048
sns <scott.schroeder@psaudio.com>
2023-09-05 11:41:37 -0600

    Adds styling and layout for the contact form

--- src/Components/ContactForm/ContactForm.tsx
+++ src/Components/ContactForm/ContactForm.tsx
@@ -8,30 +8,35 @@ const ContactForm: React.FC = () => {
 
   return (
         <section className="contact-container">
-            <form className="login-form" onSubmit={(e => handleSubmit(e))}>
-                <label htmlFor="name-input">Name</label>
-                <input
-                    type="text"
-                    id="name-input"
-                    name="name"
-                    required
-                />
-                <label htmlFor="email-input">Email</label>
-                <input
-                    type="email"
-                    id="email-input"
-                    name="email"
-                    required
-                />
-                <label htmlFor="question-input">Please feel free to reach me with any questions or opportunities</label>
-                <input
-                    type="textarea"
-                    id="question-input"
-                    name="question"
-                    maxLength={2000}
-                    required
-                />
-
+            <h3>Contact Me</h3>
+            <form className="contact-form" onSubmit={(e => handleSubmit(e))}>
+                <label htmlFor="name-input">Name:
+                    <input
+                        type="text"
+                        id="name-input"
+                        name="name"
+                        required
+                    />
+                </label>
+                <label htmlFor="email-input">Email:
+                    <input
+                        type="email"
+                        id="email-input"
+                        name="email"
+                        required
+                    />
+                </label>
+                <label htmlFor="question-input">Reach me anytime with any questions or opportunities
+                    <textarea
+                        id="question-input"
+                        name="question"
+                        rows={6}
+                        cols={63}
+                        maxLength={2000}
+                        required
+                    />
+                </label>
+                <button className="contact-button" type="submit">SEND</button>
             </form>
         </section>
   );
