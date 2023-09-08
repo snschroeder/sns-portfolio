55f9ac39d59295355aa4c40c383daef66d21a584
sns <scott.schroeder@psaudio.com>
2023-09-08 12:04:15 -0600

    WIP adds particle generation

--- src/Components/ParticleCanvas/ParticleCanvas.tsx
+++ src/Components/ParticleCanvas/ParticleCanvas.tsx
@@ -1,11 +1,26 @@
 import React, { useEffect, useRef, useState } from 'react';
 import './ParticleCanvas.css';
 
+interface ParticleInterface {
+  x: number,
+  y: number,
+  size: number,
+  trajectory: number,
+  speed: number,
+  t: number,
+  strokeColor: string,
+  particleTrailWidth: number,
+  floatAround: () => void,
+}
+
 const ParticleCanvas: React.FC = () => {
   const [windowX, setWindowX] = useState(innerWidth);
   const [windowY, setWindowY] = useState(innerHeight);
+  //   const [particleArray, setParticleArray] = useState(new Array<ParticleInterface>);
 
   const canvasRef: React.Ref<any> = useRef();
+
+  let particleArray = new Array<ParticleInterface>;
 
 
   const setSize = (): void => {
@@ -13,30 +28,50 @@ const ParticleCanvas: React.FC = () => {
     setWindowY(innerHeight);
   };
 
-  const drawParticle = (ctx: any, startX: number, startY: number, size: number) => {
-    ctx.fillStyle = 'rgba(200, 200, 0, 1)';
-    const circle = new Path2D();
-    circle.arc(startX, startY, size, 0, 2 * Math.PI);
-    ctx.fill(circle);
-  };
+  function Particle(this: ParticleInterface, ctx: any, startX: number, startY: number, size: number) {
+    this.x = startX;
+    this.y = startY;
+    this.speed = 0.02;
+    this.trajectory = Math.random() * Math.PI * 2;
+    this.t = Math.random() * 150;
+    this.strokeColor = 'rgba(200, 200, 0, 1)';
+    this.particleTrailWidth = size;
 
-  const generateParticleField = (): void => {
+    this.floatAround = () => {
+      const ls = {
+        x: this.x,
+        y: this.y,
+      };
+      this.trajectory += this.speed;
+      this.x = Math.cos(this.trajectory) * this.t;
+      this.y = Math.sin(this.trajectory) * this.t;
+      ctx.beginPath();
+      ctx.lineWidth = this.particleTrailWidth;
+      ctx.strokeStyle = this.strokeColor;
+      ctx.moveTo(ls.x, ls.y);
+      ctx.lineTo(this.x, this.y);
+      ctx.stroke();
+    };
+  }
+
+  const generateParticleField = (amount: number): void => {
     const ctx = canvasRef.current.getContext('2d');
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.55)';
+    ctx.fillRect(0, 0, windowX, windowY);
 
-    ctx.globalCompositeOperation = 'destination-over';
-    ctx.clearRect(0, 0, windowX, windowY);
-
-    drawParticle(ctx, 10, 20, 4);
-    ctx.save();
-
+    for (let i = 0; i < amount; i += 1) {
+      particleArray[i] = new (Particle as any)(ctx, 300, 300, 10);
+    }
   };
 
   const animate = () => {
-    window.requestAnimationFrame(generateParticleField);
+    requestAnimationFrame(animate);
+    particleArray.forEach((particle) => particle.floatAround());
   };
 
   useEffect(() => {
     addEventListener('resize', () => setSize());
+    generateParticleField(10);
     animate();
   }, []);
  
