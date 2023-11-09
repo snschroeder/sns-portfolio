686e696f0d5a951638cfa7087984be462b06bc97
sns <scott.schroeder@psaudio.com>
2023-11-09 16:06:27 -0700

    Adds boundary reversal so particles do not float off-screen

--- src/Components/ParticleCanvas/Particle.ts
+++ src/Components/ParticleCanvas/Particle.ts
@@ -33,6 +33,16 @@ class Particle {
 
   floatAround(): void {
     this.draw();
+
+    // Flip the direction if we hit the left or right boundary
+    if ((this.x + this.veloX) > this.windowX || (this.x + this.veloX) < 0) {
+      this.veloX = - this.veloX;
+    }
+    // Flip the direction if we hit the top or bottom boundary
+    if ((this.y + this.veloY) > this.windowY || (this.y + this.veloY) < 0)  {
+      this.veloY = - this.veloY;
+    }
+    
     this.x += this.veloX;
     this.y += this.veloY;
   }
