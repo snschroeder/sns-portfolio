3155ed79fa66c49832aa13d00798763469b9722c
sns <scott.schroeder@psaudio.com>
2023-11-27 15:44:40 -0700

    Adds GallerySlide component, WIP wiring up the slides

--- src/Components/Gallery/Gallery.tsx
+++ src/Components/Gallery/Gallery.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 
+import GallerySlide from '../GallerySlide/GallerySlide';
+
 import './Gallery.css';
 
 const Gallery: React.FC = () => {
   return (
         <section className="gallery-container">
-            <img src="https://placekitten.com/g/200/200" />
+          <GallerySlide 
+            isLeftSlide={true}
+            title='test'
+            imgLink='http://placekitten.com/g/400/400'
+            description='It is a thing!'
+            techStack='cat'
+          />
+          <GallerySlide
+            isLeftSlide={false}
+            title='test two'
+            imgLink='http://placekitten.com/g/400/400'
+            description='It is another thing!'
+            techStack='cat cat cat'         
+          />
         </section>
   );
 };
