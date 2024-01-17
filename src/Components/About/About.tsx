aab5d6aa8a7c2a8fcefa99b54c699d4ac6917199
sns <scott.schroeder@psaudio.com>
2024-01-17 15:06:06 -0700

    WIP, updates default about text

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -15,7 +15,7 @@ const About: React.FC = () => {
   const defaultAbout = {
     imgLink: 'http://placekitten.com/g/300/400',
     header: 'About me',
-    body1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    body1: 'I have been working professionally as a full stack developer since 2019, with a focus on React, TypeScript, Node.js, and both PostgreSQL and MySQL. Along the way I have enjoyed opportunities to expand my repertoire to also include PHP and WordPress, Shopify and Liquid, and Python. In this time, I have built several APIs and applications designed to connect users to their data, improve product reliability, and drive sales.',
     body2: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   };
 
