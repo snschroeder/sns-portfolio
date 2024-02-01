fda2ceda857f7c6c589aabc09c22a03e88ee0d1a
sns <scott.schroeder@psaudio.com>
2024-01-31 18:44:45 -0700

    Adds .env, updates config file

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000',
+  API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT,
   TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY || 'api-token',
   API_USERNAME: process.env.REACT_APP_API_USERNAME,
   API_PASSWORD: process.env.REACT_APP_API_PASSWORD,
